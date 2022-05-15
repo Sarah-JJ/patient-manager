414b58902d972c89bc8674ee4ac994f442b1f732
Sarah <sjuhayn@earthlink.iq>
2022-05-15 12:52:52 +0300

    feat: add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'patients',
+    children: [
+      {
+        path: '',
+        component: PageNotFoundComponent
+      },
+      {
+        path: ':id',
+        component: PageNotFoundComponent
+      }
+    ],
+    component: PageNotFoundComponent
+  },
+  { path: '',   redirectTo: '/patients', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
